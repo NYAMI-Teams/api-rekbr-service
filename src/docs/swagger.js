cbca096eb1f2dadbe7b0b1f2f6d2b7eec72c1904
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2025-06-16 14:40:18 +0700

    chore: change server ip in swagger docs

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,37 +1,37 @@
-import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerJSDoc from "swagger-jsdoc";
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API Rekbr Service',
-            version: '1.0.0',
-            description: 'API documentation for Rekbr App',
-        },
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                }
-            }
-        },
-        security: [
-            {
-                bearerAuth: [],
-            }
-        ],
-        servers: [
-            {
-                url: 'https://kvnpp4pb-3000.asse.devtunnels.ms', // sesuai base URL server
-            },
-            {
-                url: 'http://localhost:3000', // sesuai base URL server
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Rekbr Service",
+      version: "1.0.0",
+      description: "API documentation for Rekbr App",
     },
-    apis: ['./src/**/*.js'], // lokasi file dengan anotasi Swagger (pakai JSDoc-style)
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    servers: [
+      {
+        url: "http://152.42.249.176:3000", // sesuai base URL server
+      },
+      {
+        url: "http://localhost:3000", // sesuai base URL server
+      },
+    ],
+  },
+  apis: ["./src/**/*.js"], // lokasi file dengan anotasi Swagger (pakai JSDoc-style)
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
